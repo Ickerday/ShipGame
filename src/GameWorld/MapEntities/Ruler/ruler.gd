09c7e5bf1683771c95a07ecb1564b2ff6bf57914
Bartosz Jędrecki <bartoszjedrecki@gmail.com>
2023-07-28 20:21:38 +0200

    Move files around

---
+++ src/GameWorld/MapEntities/Ruler/ruler.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+class_name MapRuler
+
+var point_scene := preload("res://src/GameWorld/MapEntities/Ruler/ruler_point.tscn")
+
+
+@onready var first_point := $ruler_point
+
+var is_first_point_settled = false
+var second_point: Node2D
+
+signal ruler_placed
+
+###############################################################################
+# Builtin functions                                                           #
+###############################################################################
+
+
+func _draw():
+	if second_point:
+		draw_dashed_line(Vector2.ZERO, to_local(second_point.global_position), Color.WHITE, 4.0, true)
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		if !second_point:
+			global_position = get_global_mouse_position()
+		else:
+			second_point.global_position = get_global_mouse_position()
+			queue_redraw()
+	if event is InputEventMouseButton and event.button_mask == MOUSE_BUTTON_LEFT:
+		if !second_point:
+			create_second_point()
+		else:
+			set_process_unhandled_input(false)
+			ruler_placed.emit()
+
+
+###############################################################################
+# Public functions                                                            #
+###############################################################################
+
+func create_second_point():
+	is_first_point_settled = true
+	second_point = point_scene.instantiate()
+	add_child(second_point)
+
+###############################################################################
+# Connections                                                                 #
+###############################################################################
+
+###############################################################################
+# Private functions                                                           #
+###############################################################################
---
+++ src/UserGameInterface/ContextMenuRight/MissileContextMenu/missile_context_menu.gd
@@ -0,0 +1,62 @@
+extends Control
+
+class_name MissileContextMenu
+
+var inventory_ref: WeakRef
+var torpedo_launcher_ref: WeakRef
+var missile_button_scene := preload("res://src/UserGameInterface/ContextMenuRight/MissileContextMenu/MissileButton.tscn")
+@onready var missile_container := $VBoxContainer
+
+###############################################################################
+# Builtin functions                                                           #
+###############################################################################
+
+func _ready():
+	await get_tree().process_frame
+	var player_ref = InputMediator.player
+	assert(player_ref)
+	var player: CharacterBody2D = player_ref.get_ref()
+	inventory_ref = weakref(player.get_node("InventoryComponent"))
+	torpedo_launcher_ref = weakref(player.get_node("TorpedoLauncherComponent"))
+
+###############################################################################
+# Public functions                                                            #
+###############################################################################
+
+func activate():
+	var inventory: InventoryComponent = inventory_ref.get_ref()
+	if not inventory:
+		print("couldn't find any inventory to display!")
+		return
+	_sync_missiles_with_inventory(inventory)
+	show()
+
+
+func deactivate():
+	hide()
+
+###############################################################################
+# Connections                                                                 #
+###############################################################################
+
+###############################################################################
+# Private functions                                                           #
+###############################################################################
+
+func _is_item_displayed(item: InventoryItem):
+	# todo – store hashes or ids instead of checking for all the references…
+	for missile in missile_container.get_children():
+		if missile.item == item:
+			return true
+	return false
+
+func _sync_missiles_with_inventory(inventory: InventoryComponent):
+	var torpedo_launcher = torpedo_launcher_ref.get_ref()
+	for i in inventory.items.size():
+		var item = inventory.items[i]
+		if item.item_type == InventoryItem.ItemType.Missile and !_is_item_displayed(item):
+			var new_missile = missile_button_scene.instantiate()
+			new_missile.item = item
+			missile_container.add_child(new_missile)
+			new_missile.button.pressed.connect(torpedo_launcher.load_torpedo.bind(item))
+			new_missile.button.pressed.connect(InputMediator.request_interface_state_change.bind(GameInputMediator.InterfaceState.MissileLaunch))
