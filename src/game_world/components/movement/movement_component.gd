367a71ed2573e18f8563fa9d844a50fb166b493f
Bartosz Jędrecki <bartoszjedrecki@gmail.com>
2023-08-11 04:45:14 +0200

    Begin implementing automatic movement mode in MovementComponent

--- src/game_world/components/movement/movement_component.gd
+++ src/game_world/components/movement/movement_component.gd
@@ -4,17 +4,17 @@ class_name MovementComponent
 ###############################################################################
 # EDITOR                                                                      #
 ###############################################################################
-# for drawing lines, displaying extra data and whatnot
+# For drawing lines, displaying extra data and whatnot
 @export var debug := false
 
 ###############################################################################
-# references                                                                  #
+# References                                                                  #
 ###############################################################################
 
 var parent: CharacterBody2D
 
 ###############################################################################
-# exports                                                                     #
+# Exports                                                                     #
 ###############################################################################
 
 @export var max_speed: float = 150.0
@@ -23,17 +23,28 @@ var parent: CharacterBody2D
 @export var rotation_speed: float = 0.5
 
 ###############################################################################
-# class-related variables                                                     #
+# Class-related variables                                                     #
 ###############################################################################
 
-var desired_speed: float = 0 : set = set_desired_speed
-var desired_rotation: float = 0 : set = set_desired_rotation
+var desired_speed: float = 0:
+	set = set_desired_speed
+var desired_rotation: float = 0:
+	set = set_desired_rotation
+
+enum MovementMode { MANUAL = 0, AUTO = 1 }
+var movement_mode: MovementMode = MovementMode.MANUAL
+
+# TODO: "destination"?
+var desired_position: Vector2 = Vector2.ZERO:
+	set = set_desired_position
+
 var velocity: Vector2 = Vector2.ZERO
 var current_speed: float = 0.0
 
 signal body_moved(velocity: Vector2)
 signal desired_speed_changed(new_speed: Vector2)
 signal desired_rotation_changed(new_rotation: Vector2)
+signal desired_position_changed(new_location: Vector2)
 
 ###############################################################################
 # Setters                                                                     #
@@ -45,9 +56,15 @@ func set_desired_speed(new_speed: float):
 	desired_speed_changed.emit(desired_speed)
 
 
-func set_desired_rotation(new_rotation):
+func set_desired_rotation(new_rotation: float):
 	desired_rotation = max(min(new_rotation, max_rotation), -max_rotation)
 	desired_rotation_changed.emit(desired_rotation)
+
+
+func set_desired_position(new_location: Vector2):
+	movement_mode = MovementMode.AUTO
+	desired_position = new_location
+	desired_position_changed.emit(desired_position)
 
 
 ###############################################################################
@@ -60,11 +77,18 @@ func _ready():
 
 
 func _physics_process(delta):
-	if parent:
+	if not parent:
+		return
+
+	if movement_mode == MovementMode.MANUAL:
 		parent.set_velocity(_calculate_new_velocity(delta))
 		parent.rotation += desired_rotation * rotation_speed * delta
 		parent.move_and_slide()
 		body_moved.emit(parent.velocity)
+
+	if movement_mode == MovementMode.AUTO:
+		# ! TODO
+		pass
 
 
 ###############################################################################
@@ -85,9 +109,11 @@ func at_new_desired_rotation(new_rotation: float, hard_set: bool = false):
 	else:
 		self.desired_rotation += new_rotation
 
+
 ###############################################################################
 # Private functions                                                           #
 ###############################################################################
+
 
 func _calculate_new_velocity(_delta: float) -> Vector2:
 	current_speed = min(lerp(current_speed, desired_speed, acceleration), max_speed)
